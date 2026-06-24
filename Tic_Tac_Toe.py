4242d27c3a715d6124af9e365fa1846c572ea186
Ritayan Raychaudhuri <ritayanrc2008@gmail.com>
2026-06-24 14:16:28 +0530

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -25,7 +25,7 @@ while c:
     else:
         a=input("Enter Player 2 name: ")
         k=input("Enter Player 1 name: ")
-    print(L)
+    print(tabulate(L,tablefmt="grid"))
     D={1:[0,0],2:[0,1],3:[0,2],4:[1,0],5:[1,1],6:[1,2],7:[2,0],8:[2,1],9:[2,2]}
     if b==True:
         while True:
